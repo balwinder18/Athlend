adcdc0d48f8394009e420afa78e3b79e2affe98b
balwinder18 <144101609+balwinder18@users.noreply.github.com>
2025-03-26 00:22:37 +0530

    removed build error

---
+++ app/api/groundDetails/[id]/route.js
@@ -0,0 +1,63 @@
+// import { connecttodatabase } from "@/database/db";
+// import Grounds from "@/database/models/GroundsModel";
+// import { NextResponse } from "next/server";
+
+
+
+// export async function GET( req: Request,
+//     { params }: { params: { id: string } }){
+
+//     try {
+        
+//         await connecttodatabase();
+
+//         const id =await params.id;
+//         if(!id){
+//             return NextResponse.json({message:"provide gorund id"} , {status :500})
+//         }
+
+//         const data =await Grounds.findById(id);
+//         if(!data){
+//             return NextResponse.json({message:"gorund details not found"} , {status :400})
+            
+//         }
+
+//         return NextResponse.json(data , {status :200})
+        
+//     } catch (error) {
+//         return NextResponse.json(error, {status :404})
+        
+//     }
+
+// } 
+
+
+import { connecttodatabase } from "@/database/db";
+import Grounds from "@/database/models/GroundsModel";
+import { NextResponse } from "next/server";
+
+export async function GET(req, { params }) {
+    try {
+        await connecttodatabase();
+
+        const id = params.id;
+        if (!id) {
+            return NextResponse.json(
+                { message: "provide ground id" },
+                { status: 500 }
+            );
+        }
+
+        const data = await Grounds.findById(id);
+        if (!data) {
+            return NextResponse.json(
+                { message: "ground details not found" },
+                { status: 400 }
+            );
+        }
+
+        return NextResponse.json(data, { status: 200 });
+    } catch (error) {
+        return NextResponse.json(error, { status: 404 });
+    }
+}
