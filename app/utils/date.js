4cc89c978f593f321e53c4af8f57b768ae469381
balwinder18 <144101609+balwinder18@users.noreply.github.com>
2025-04-03 00:53:37 +0530

    1

--- app/utils/date.js
+++ app/utils/date.js
@@ -1,5 +1,5 @@
 import { format } from 'date-fns-tz';
 
 export const formatLocalTime = (date, timezone, pattern = 'HH:mm') => {
-  return format(new Date(date), pattern, { timeZone: timezone });
+  return format(new Date(date), pattern, { timeZone: "Asia/Kolkata" });
 };
