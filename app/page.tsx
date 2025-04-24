5c30f818903c2d4f501b4dda384830e62749f0ab
balwinder18 <144101609+balwinder18@users.noreply.github.com>
2025-04-25 00:59:33 +0530

    added meta tag

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,20 @@ import Locationdetect from '../components/Locationdetect';
 import Bodycontent from '../components/home/Bodycontent'
 import Allgrounds from '../components/home/Allgrounds'
 import Help from '../components/Help'
+import Head from "next/head";
 
 export default function Home() {
  
 
   return (
     <>
+    <Head>
+        <title>Athlend: making gorunds search easier</title>
+        <meta
+          name="description"
+          content="Find you best sports ground nearby."
+        />
+</Head>
     
       <Navbar/>
       <Bodycontent/>
